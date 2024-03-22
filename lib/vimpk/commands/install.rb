454b7ba96960f8654bbe76dd08b7a09d8a2a8664
Piotr Usewicz <piotr@layer22.com>
2024-03-22 13:21:10 +0100

    chore: Extract commands into a Commands module

---
+++ lib/vimpk/commands/install.rb
@@ -0,0 +1,27 @@
+module VimPK
+  module Commands
+    class Install
+      include VimPK::Colorizer
+
+      PackageExistsError = Class.new(StandardError)
+
+      attr_reader :dest
+
+      def initialize(package, options)
+        @package = package || raise(ArgumentError, "Package name is required")
+        @path = options.path
+        @pack = options.pack || options.default_pack
+        @type = options.type || options.default_type
+        @dest = File.join(@path, @pack, @type, File.basename(@package))
+        @source = "https://github.com/#{package}.git"
+        @git = Git
+      end
+
+      def call
+        raise PackageExistsError, "Package #{@package} already exists at #{@dest}" if File.exist?(@dest)
+
+        Git.clone(@source, @dest, dir: @path)
+      end
+    end
+  end
+end
